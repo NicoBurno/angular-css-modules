c9a893ab9d4ecfc2188fc51c1a4ab865de19a1f3
NicoBurno <nicoburno@gmail.com>
2017-06-09 13:05:50 +0000

    init module

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as angular from 'angular';
+
+const module = angular
+	.module("angular-css-modules", [])
+
+export default module.name;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+
+const srcPath = path.join(__dirname, './src');
+const distPath = path.join(__dirname, './dist');
+
+const rules = [
+	{
+		test: /\.js$/,
+		loader: 'babel-loader',
+		exclude: /node_modules/
+	}
+];
+
+const plugins = [];
+
+if (isProduction) {
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true
+		})
+	);
+}
+
+var config = module.exports = {
+	context: srcPath,
+	entry: './index.js',
+	output: {
+		path: distPath,
+		filename: 'angular-css-modules.js',
+		library: 'angular-css-modules',
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	},
+	devtool: isProduction ? 'source-map' : 'eval',
+	watch: !isProduction,
+	module: {
+		rules
+	},
+	plugins,
+	externals: [
+		'angular'
+	]
+};
